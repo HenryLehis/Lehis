65a4e288b29d5f7b7dd63615175c3e1ceafc5d41
= <=>
2022-04-07 13:14:01 +0300

    Routes

--- routes/web.php
+++ routes/web.php
@@ -17,20 +17,28 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
     $files = File::files(resource_path("posts"));
-    $documents = [];
 
-    foreach ($files as $file){
-       $documents = YamlFrontMatter::parseFile($file);
-    }
+    $posts = collect($files)
+        ->map(function($file){
+            $document = YamlFrontMatter::parseFile($file);
 
+       $posts[] = new Post(
+           $document->title,
+           $document->excerpt,
+           $document->date,
+           $document->body(),
+           $document->slug
+       );
 
-        return view('posts', [
-            "posts"=>Post::all()
+        });
+
+    return view('posts', [
+        'posts' => $posts
         ]);
     });
     Route::get('/posts/{post}', function ($slug){
         return view('post',[
-            'post' => Post::find($slug)
+            'post' => Post::findOrFail($slug)
         ]);
-    })->where('post', '[A-z_\-]+');
+    });
 
