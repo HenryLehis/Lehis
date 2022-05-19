0c4b7c8e25fa994320c25a8f40bbef5dd9605055
= <=>
2022-05-19 09:59:29 +0300

    Section 2 episone 12

--- routes/web.php
+++ routes/web.php
@@ -16,22 +16,16 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $files = File::files(resource_path("posts"));
-
-
-    $posts = collect($files)
-        ->map(function($file){
-            $document = YamlFrontMatter::parseFile($file);
-            //dd($document);
-       return new Post(
+$posts = collect(File::files(resource_path("posts")))
+    ->map(fn($file)=>YamlFrontMatter::parseFile($file))
+    ->map(fn($document) => new Post (
            $document->title,
            $document->excerpt,
            $document->date,
            $document->body(),
            $document->slug
-       );
+       ));
 
-        });
     return view('posts', [
         'posts' => $posts
         ]);
