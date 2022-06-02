1397e023ab74fe3e5545976c94e63c60d8e0c5e2
= <=>
2022-06-02 14:24:41 +0300

    db

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +17,60 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        User::truncate();
+        Post::truncate();
+        Category::truncate();
+
+
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category' => $family->id,
+            'title' => 'My Family Post',
+            'slug' => 'my-family-post',
+            'excerpt' => 'Lorem ipsum dolar sit amet.',
+            'body' => 'Lorem ipsum dolor sit amet. Ut Quis sapiente id soluta harum ut fugit esse sed aspernatur Quis aut autem voluptas non officiis assumenda aut quia exercitationem. Qui neque consequatur vel consectetur voluptas vel aliquid eaque est ipsa quam.
+
+            Ad officia nobis nam tempora maiores aut iste delectus sit quos similique sit quis facere At voluptatem. Eum odit minima eos alias deserunt hic unde odio aut labore saepe et culpa consequatur omnis repellat id nihil porro. Eos maxime ipsa aut totam reprehenderit ut officia ipsa.
+
+            Sit rerum velit non rerum delectus sit laboriosam reiciendis ut impedit quos quo obcaecati laboriosam. Ea maiores harum ut mollitia quidem ab quis commodi. Non dolor dolorum id quaerat fugit ut pariatur numquam ut cupiditate illum ad praesentium impedit est laudantium inventore. Ea quidem molestiae et alias dignissimos sit nulla rerum.'
+
+        ]);
+
+
+        Post::create([
+            'user_id' => $user->id,
+            'category' => $work->id,
+            'title' => 'My Work Post',
+            'slug' => 'my-work-post',
+            'excerpt' => 'Lorem ipsum dolar sit amet.',
+            'body' => 'Lorem ipsum dolor sit amet. Ut Quis sapiente id soluta harum ut fugit esse sed aspernatur Quis aut autem voluptas non officiis assumenda aut quia exercitationem. Qui neque consequatur vel consectetur voluptas vel aliquid eaque est ipsa quam.
+
+            Ad officia nobis nam tempora maiores aut iste delectus sit quos similique sit quis facere At voluptatem. Eum odit minima eos alias deserunt hic unde odio aut labore saepe et culpa consequatur omnis repellat id nihil porro. Eos maxime ipsa aut totam reprehenderit ut officia ipsa.
+
+            Sit rerum velit non rerum delectus sit laboriosam reiciendis ut impedit quos quo obcaecati laboriosam. Ea maiores harum ut mollitia quidem ab quis commodi. Non dolor dolorum id quaerat fugit ut pariatur numquam ut cupiditate illum ad praesentium impedit est laudantium inventore. Ea quidem molestiae et alias dignissimos sit nulla rerum.'
+
+        ]);
+
+
+
     }
 }
